9cda3d9857da9e55673c008db3cafdaf2fb2e521
GalioV <galilea.valle@gmail.com>
2019-02-20 13:26:36 -0600

    Creamos la funciones para cada botón de tipo y agregamos contenido de texto a Cómo Jugar

--- src/index.js
+++ src/index.js
@@ -126,13 +126,10 @@ function showPokemons(){
 	}	
 }
 
-let grass = document.getElementById("typeGrass");
-//document.getElementById("typeGrass")
-grass.addEventListener("click",showFilter);
-//grass.addEventListener("click", function (){
-	//console.log(window.filterType(POKEMON.pokemon, grass.value));
+const grass = document.getElementById("typeGrass");
+grass.addEventListener("click",showFilterGrass);
 
-function showFilter(){
+function showFilterGrass(){
 	const typeResult = window.filterType(POKEMON.pokemon, grass.value);
 	const pokemonNames = typeResult.map((pokemon)=> {
 	return pokemon.name;
@@ -140,3 +137,178 @@ function showFilter(){
 	document.getElementById('showTypes').innerHTML = pokemonNames;
 };
 
+const water = document.getElementById("typeWater");
+water.addEventListener("click",showFilterWater);
+
+function showFilterWater(){
+	const typeResult = window.filterType(POKEMON.pokemon, water.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const ground = document.getElementById("typeGround");
+ground.addEventListener("click",showFilterGround);
+
+function showFilterGround(){
+	const typeResult = window.filterType(POKEMON.pokemon, ground.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const psychic = document.getElementById("typePsychic");
+psychic.addEventListener("click",showFilterPsychic);
+
+function showFilterPsychic(){
+	const typeResult = window.filterType(POKEMON.pokemon, psychic.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const ghost = document.getElementById("typeGhost");
+ghost.addEventListener("click",showFilterGhost);
+
+function showFilterGhost(){
+	const typeResult = window.filterType(POKEMON.pokemon, ghost.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const dark = document.getElementById("typeDark");
+dark.addEventListener("click",showFilterDark);
+
+function showFilterDark(){
+	const typeResult = window.filterType(POKEMON.pokemon, dark.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const poison = document.getElementById("typePoison");
+poison.addEventListener("click",showFilterPoison);
+
+function showFilterPoison(){
+	const typeResult = window.filterType(POKEMON.pokemon, poison.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const fighting = document.getElementById("typeFighting");
+fighting.addEventListener("click",showFilterFighting);
+
+function showFilterFighting(){
+	const typeResult = window.filterType(POKEMON.pokemon, fighting.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const steel = document.getElementById("typeSteel");
+steel.addEventListener("click",showFilterSteel);
+
+function showFilterSteel(){
+	const typeResult = window.filterType(POKEMON.pokemon, steel.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const bug = document.getElementById("typeBug");
+bug.addEventListener("click",showFilterBug);
+
+function showFilterBug(){
+	const typeResult = window.filterType(POKEMON.pokemon, bug.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const fire = document.getElementById("typeFire");
+fire.addEventListener("click",showFilterFire);
+
+function showFilterFire(){
+	const typeResult = window.filterType(POKEMON.pokemon, fire.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const dragon = document.getElementById("typeDragon");
+dragon.addEventListener("click",showFilterDragon);
+
+function showFilterDragon(){
+	const typeResult = window.filterType(POKEMON.pokemon, dragon.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const flying = document.getElementById("typeFlying");
+flying.addEventListener("click",showFilterFlying);
+
+function showFilterFlying(){
+	const typeResult = window.filterType(POKEMON.pokemon, flying.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const normal = document.getElementById("typeNormal");
+normal.addEventListener("click",showFilterNormal);
+
+function showFilterNormal(){
+	const typeResult = window.filterType(POKEMON.pokemon, normal.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const electric = document.getElementById("typeElectric");
+electric.addEventListener("click",showFilterElectric);
+
+function showFilterElectric(){
+	const typeResult = window.filterType(POKEMON.pokemon, electric.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const rock = document.getElementById("typeRock");
+rock.addEventListener("click",showFilterRock);
+
+function showFilterRock(){
+	const typeResult = window.filterType(POKEMON.pokemon, rock.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
+
+const ice = document.getElementById("typeIce");
+ice.addEventListener("click",showFilterIce);
+
+function showFilterIce(){
+	const typeResult = window.filterType(POKEMON.pokemon, ice.value);
+	const pokemonNames = typeResult.map((pokemon)=> {
+	return pokemon.name;
+	});
+	document.getElementById('showTypes').innerHTML = pokemonNames;
+};
