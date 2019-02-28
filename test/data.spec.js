d23e3f83cb7a72a4171bf4d5f8e8838aa7ff19af
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-02-28 10:44:04 -0600

    función habilitada y test corriendo, tambien se generó un JSON por cada respuesta de las funciones de ordenado alfabetico

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,196 +1,8 @@
 //const pokemon = require ('../src/data/pokemon/pokemon.json');
 require('../src/data.js');
-const pokemonFilterMock = require('../src/data/pokemonFilter.json');
-
-//el mock debe ir aquí?  y es esto? 
-  const POKEMON = {
-  "pokemon": [{
-    "id": 1,
-    "num": "001",
-    "name": "Bulbasaur",
-    "img": "http://www.serebii.net/pokemongo/pokemon/001.png",
-    "type": [
-      "Grass",
-      "Poison"
-    ],
-    "height": "0.71 m",
-    "weight": "6.9 kg",
-    "candy": "Bulbasaur Candy",
-    "candy_count": 25,
-    "egg": "2 km",
-    "spawn_chance": 0.69,
-    "avg_spawns": 69,
-    "spawn_time": "20:00",
-    "multipliers": [1.58],
-    "weaknesses": [
-      "Fire",
-      "Ice",
-      "Flying",
-      "Psychic"
-    ],
-    "next_evolution": [{
-      "num": "002",
-      "name": "Ivysaur"
-    }, {
-      "num": "003",
-      "name": "Venusaur"
-    }]
-  }, {
-    "id": 2,
-    "num": "002",
-    "name": "Ivysaur",
-    "img": "http://www.serebii.net/pokemongo/pokemon/002.png",
-    "type": [
-      "Grass",
-      "Poison"
-    ],
-    "height": "0.99 m",
-    "weight": "13.0 kg",
-    "candy": "Bulbasaur Candy",
-    "candy_count": 100,
-    "egg": "Not in Eggs",
-    "spawn_chance": 0.042,
-    "avg_spawns": 4.2,
-    "spawn_time": "07:00",
-    "multipliers": [
-      1.2,
-      1.6
-    ],
-    "weaknesses": [
-      "Fire",
-      "Ice",
-      "Flying",
-      "Psychic"
-    ],
-    "prev_evolution": [{
-      "num": "001",
-      "name": "Bulbasaur"
-    }],
-    "next_evolution": [{
-      "num": "003",
-      "name": "Venusaur"
-    }]
-  }, {
-    "id": 3,
-    "num": "003",
-    "name": "Venusaur",
-    "img": "http://www.serebii.net/pokemongo/pokemon/003.png",
-    "type": [
-      "Grass",
-      "Poison"
-    ],
-    "height": "2.01 m",
-    "weight": "100.0 kg",
-    "candy": "Bulbasaur Candy",
-    "egg": "Not in Eggs",
-    "spawn_chance": 0.017,
-    "avg_spawns": 1.7,
-    "spawn_time": "11:30",
-    "multipliers": null,
-    "weaknesses": [
-      "Fire",
-      "Ice",
-      "Flying",
-      "Psychic"
-    ],
-    "prev_evolution": [{
-      "num": "001",
-      "name": "Bulbasaur"
-    }, {
-      "num": "002",
-      "name": "Ivysaur"
-    }]
-  }, {
-    "id": 4,
-    "num": "004",
-    "name": "Charmander",
-    "img": "http://www.serebii.net/pokemongo/pokemon/004.png",
-    "type": [
-      "Fire"
-    ],
-    "height": "0.61 m",
-    "weight": "8.5 kg",
-    "candy": "Charmander Candy",
-    "candy_count": 25,
-    "egg": "2 km",
-    "spawn_chance": 0.253,
-    "avg_spawns": 25.3,
-    "spawn_time": "08:45",
-    "multipliers": [1.65],
-    "weaknesses": [
-      "Water",
-      "Ground",
-      "Rock"
-    ],
-    "next_evolution": [{
-      "num": "005",
-      "name": "Charmeleon"
-    }, {
-      "num": "006",
-      "name": "Charizard"
-    }]
-  }, {
-    "id": 5,
-    "num": "005",
-    "name": "Charmeleon",
-    "img": "http://www.serebii.net/pokemongo/pokemon/005.png",
-    "type": [
-      "Fire"
-    ],
-    "height": "1.09 m",
-    "weight": "19.0 kg",
-    "candy": "Charmander Candy",
-    "candy_count": 100,
-    "egg": "Not in Eggs",
-    "spawn_chance": 0.012,
-    "avg_spawns": 1.2,
-    "spawn_time": "19:00",
-    "multipliers": [1.79],
-    "weaknesses": [
-      "Water",
-      "Ground",
-      "Rock"
-    ],
-    "prev_evolution": [{
-      "num": "004",
-      "name": "Charmander"
-    }],
-    "next_evolution": [{
-      "num": "006",
-      "name": "Charizard"
-    }]
-  }, {
-    "id": 6,
-    "num": "006",
-    "name": "Charizard",
-    "img": "http://www.serebii.net/pokemongo/pokemon/006.png",
-    "type": [
-      "Fire",
-      "Flying"
-    ],
-    "height": "1.70 m",
-    "weight": "90.5 kg",
-    "candy": "Charmander Candy",
-    "egg": "Not in Eggs",
-    "spawn_chance": 0.0031,
-    "avg_spawns": 0.31,
-    "spawn_time": "13:34",
-    "multipliers": null,
-    "weaknesses": [
-      "Water",
-      "Electric",
-      "Rock"
-    ],
-    "prev_evolution": [{
-      "num": "004",
-      "name": "Charmander"
-    }, {
-      "num": "005",
-      "name": "Charmeleon"
-    }]
-  }
-]};
-
+const POKEMON = require('../src/data/pokemon/pokemon.json');
+const pokemonFilterOrderMock = require('../src/data/pokemon/filterorder.json');
+const pokemonFilterInverseOrderMock = require('../src/data/pokemon/filterInverseOrder.json');
 
 describe('filterType', () => {
 
@@ -199,25 +11,38 @@ describe('filterType', () => {
   });
 
   it('returns `filterType`', () => {
-      expect(window.filterType(POKEMON.pokemon, 'Grass').length).toBe(3);
+      expect(window.filterType(POKEMON.pokemon, 'Grass').length).toBe(14);
     });
 
   it('returns `filterType`', () => {
-    expect(window.filterType(POKEMON.pokemon, 'Dark').length).toBe(0);
+    expect(window.filterType(POKEMON.pokemon, 'Dark')).toEqual([]);
   });
 
 
 });
 
 describe('alphabeticalOrder', () => {
+
   it('it should be a function', () => {
     expect(typeof window.alphabeticalOrder).toBe('function');
-  } 
-
-  it('returns `alphabeticalOrder`', () => {
-    expect(window.filterType(POKEMON.pokemon, 'Grass')).toBe(pokemonFilterMock);
   });
+
+  it('returns alphabeticalOrder', () => {
+    expect(window.alphabeticalOrder(POKEMON.pokemon)).toEqual(pokemonFilterOrderMock);
+  });
+
   
+});
+
+describe('alphabeticalInverseOrder', () => {
+  it('it should be a function', () => {
+    expect(typeof window.alphabeticalInverseOrder).toBe('function'); 
+  });
+
+  it('returns alphabeticalOrder', () => {
+    expect(window.alphabeticalInverseOrder(POKEMON.pokemon)).toEqual(pokemonFilterInverseOrderMock);
+  });
+
 });
 //crear un archivo por cada json de respuesta
 
@@ -235,7 +60,7 @@ require(./elarchivo);
 const pokemon = require (../la ruta al archivo) 
 para el set de datos que elijamos se necesita agregar esa definición
 
-describe('pokemon', () => {it ('' )}
+describe('pokemon', () => {it ('')}
 
 API Application Programmin interface 
 un termino generico que se usa para amplia variedad de cosas... 
